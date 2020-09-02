e1404c33d83ea7bc3220ede4c395a3c80a83926c
samdan <samdan@whizzogroup.com>
2020-09-02 16:15:25 +0100

    added http request for details

--- app/src/main/java/com/ijikod/lastfm/data/api/LastFmApiService.kt
+++ app/src/main/java/com/ijikod/lastfm/data/api/LastFmApiService.kt
@@ -1,5 +1,6 @@
 package com.ijikod.lastfm.data.api
 
+import com.ijikod.lastfm.data.model.AlbumDetailsResults
 import com.ijikod.lastfm.data.model.AlbumResults
 import retrofit2.Response
 import retrofit2.http.GET
@@ -17,6 +18,13 @@ interface LastFmApiService{
     suspend fun searchAlbum(@Query("album") album: String, @Query("api_key") apiKey: String) : Response<AlbumResults>
 
 
+    /**
+     * Get [AlbumDetailsResults] from selected
+     * @param album
+     * @param artist
+     * **/
+    @GET("2.0/?method=album.getinfo&format=json")
+    suspend fun getAlbumDetails(@Query("album") album: String, @Query("artist") artist: String, @Query("api_key") apiKey: String) : Response<AlbumDetailsResults>
 
 
 }
