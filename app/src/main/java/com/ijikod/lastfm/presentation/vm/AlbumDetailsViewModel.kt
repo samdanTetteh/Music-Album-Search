3cd8c8e18880235fe82f549bea4c07489f5c144e
samdan <samdan@whizzogroup.com>
2020-09-02 17:05:11 +0100

    implemented album details view model

---
+++ app/src/main/java/com/ijikod/lastfm/presentation/vm/AlbumDetailsViewModel.kt
@@ -0,0 +1,36 @@
+package com.ijikod.lastfm.presentation.vm
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.ijikod.lastfm.data.Repository
+import com.ijikod.lastfm.data.model.Album
+import com.ijikod.lastfm.data.model.AlbumDetails
+import com.ijikod.lastfm.data.model.AlbumSearchResults
+
+class AlbumDetailsViewModel(private val repository: Repository): ViewModel() {
+
+    // Shared album item between details and search fragment
+    private val selectedAlbum : MutableLiveData<Album> = MutableLiveData<Album>()
+
+    var albumDetailsResults: LiveData<AlbumDetails> = Transformations.map(selectedAlbum){
+        repository.getAlbumsDetails(it.name, it.artist, it.mbid)
+    }
+
+    val albumsDetails : LiveData<AlbumDetails> = Transformations.switchMap(albumDetailsResults){
+        repository.albumDetails
+    }
+
+    var networkErrors: LiveData<String> = Transformations.switchMap(albumDetailsResults){
+        repository.networkErrors
+    }
+
+
+    /**
+     * Set value to Shared album [Album] used in Details Fragment
+     * **/
+    fun setSelectedVideo(album : Album){
+        selectedAlbum.value = album
+    }
+}
